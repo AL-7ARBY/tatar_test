626c8732c0361c0e59f707f13787f63fc7173fbe
Roman Kolesnev <rvkolesnev@gmail.com>
2013-02-01 22:52:18 +0400

    initail commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TatarTest::Application.config.secret_token = '3c4d7d4d15a01f88480e175a0d2b60624f058856321cf125600e67c1fbe569fc7f4b18d940b731fd268a1e92c337d8ceb3cf3778f6b682154116ad8f7dda0239'
